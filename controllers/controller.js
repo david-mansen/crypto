9ef5ea7bd9f90f832c11633dd087ec6805785ab7
marioiovanna <marioiovanna@hotmail.com>
2017-07-10 12:31:34 -0400

    onboard page created

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var model = require("../models/models.js");
 
 router.get("/", function(req, res) {
-    res.render("trade", {cryptocoins: model.cryptocoins.getData()});
+    res.render("onboard", {cryptocoins: model.cryptocoins.getData()});
 //   cat.all(function(data) {
 //     var hbsObject = {
 //       cats: data
