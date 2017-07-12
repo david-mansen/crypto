1b0737e1de9e3449f39c549b44e157ce33a014af
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-07-12 11:20:23 -0400

    API calls all working, drafted, moving to seperate js file

--- models/user.js
+++ models/user.js
@@ -14,12 +14,13 @@ var userSchema = mongoose.Schema({
 //generate hash
 userSchema.methods.generateHash = function(password){
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-}
+};
 
 //check if password is valid
 userSchema.methods.validPassword = function(password){
     return bcrypt.compareSync(password, this.local.password);
-}
+};
 
 //create model
 module.exports = mongoose.model('User', userSchema);
+
