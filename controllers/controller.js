3f3f005c7e214e20b476406b4f72636ee9b96195
marioiovanna <marioiovanna@hotmail.com>
2017-07-14 11:33:52 -0400

    suck it again kelvin

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,7 +7,7 @@ module.exports = function(app, passport){
 
     //home page
     app.get('/', function(req,res){
-        res.render("profile");
+        res.render("onboard");
     });
 
     //show login form
