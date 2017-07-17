5bd9b8a9f2b9629723d18b2f0101e389a92f5030
marioiovanna <marioiovanna@hotmail.com>
2017-07-17 06:49:46 -0400

    adasdad

--- public/assets/js/trade.js
+++ public/assets/js/trade.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    console.log('Loaded JS');
+
     $('#amount').keyup(function () {
 
         var coin = $('.coin').val();
@@ -10,18 +12,3 @@ $(document).ready(function () {
         $('#total').attr('placeholder', total);
     });
 });
-
-getData: function () {
-    app.get("/Orders", function (req, res) {
-
-        request({
-            method: 'POST',
-            url: 'https://private-eb3c42-coinigy.apiary-mock.com/api/v1/orders',
-            headers: {
-            }
-        }, function (error, response, body) {
-
-            res.render('Response:', body);
-        });
-    })
-}
